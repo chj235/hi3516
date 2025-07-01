6d82108b0e8ec5043443da2be3ef6d33087bd63d
chj235 <chj235@outlook.com>
2025-07-01 17:32:41 +0800

    Update sample_media_ai.h

--- sdk_linux/sample/taurus/ai_sample/smp/sample_media_ai.h
+++ sdk_linux/sample/taurus/ai_sample/smp/sample_media_ai.h
@@ -167,7 +167,7 @@ typedef struct AicMediaInfo {
 	
     OsdSet *osds; // OSD set for Plug to output OSD in resFrm
 } AicMediaInfo;
-
+HI_S32 SAMPLE_MEDIA_CNN_EMOTION_CLASSIFY(HI_VOID); 
 HI_S32 SAMPLE_MEDIA_CNN_TRASH_CLASSIFY(HI_VOID);
 HI_S32 SampleCommVoStartDevMipi(VO_DEV VoDev, VO_PUB_ATTR_S* pstPubAttr);
 HI_S32 SAMPLE_MEDIA_HAND_CLASSIFY(HI_VOID);
